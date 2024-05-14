b153c80377ec6a2cc70ad6ad817c3f397f7e13b7
mufid1012 <mufidghibran1012@gmail.com>
2024-05-14 15:52:20 +0700

    menambahkan void angka, isiData

--- destruktor.cpp
+++ destruktor.cpp
@@ -32,11 +32,10 @@ void angka::cetakData() {
     cout << endl;
 }
 
-int main()
-{
-    angka belajarcpp(3);   
-    angka* ptrBelajarcpp = new angka(5);
-    delete ptrBelajarcpp;      
-
-    return 0;
-}   
+void angka::isiData(){
+    for (int i=1;i<=panjang;i++) {
+        cout <<i<<" = ";
+        cin>>arr[i];
+    }
+    cout<<endl;
+}
